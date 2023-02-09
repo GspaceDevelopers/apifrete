5116d3cbc3596d6b7f9507c6cf3f2cdb045f9879
Gspace Developers <115160413+GspaceDevelopers@users.noreply.github.com>
2023-02-09 15:30:19 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
-import cors from "cors";
-export default (app) => {
-  app.use("/cep", require("./routes/cep"));
-  app.use("/frete", require("./routes/frete"));
-  app.use(cors());
-};
+const express = require("express");
+const bodyParser = require("body-parser");
+import {
+  calcularPrecoPrazo,
+} from 'correios-brasil';
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/frete", (req, res) => {
+
+  const { ceporigem, cepdestino } = req.body
+  let args = {
+    // Não se preocupe com a formatação dos valores de entrada do cep, qualquer uma será válida (ex: 21770-200, 21770 200, 21asa!770@###200 e etc),
+    sCepOrigem: ceporigem,
+    sCepDestino: cepdestino,
+    nVlPeso: '1',
+    nCdFormato: '1',
+    nVlComprimento: '1',
+    nVlAltura: '1',
+    nVlLargura: '1',
+    nCdServico: ['04014', '04510'], //Array com os códigos de serviço
+    nVlDiametro: '0',
+  };
+
+  calcularPrecoPrazo(args).then(response => {
+    console.log(response);
+
+    return res.json(response)
+  });
+
+})
+app.listen(3333, () => {
+  console.log("A API de consulta de preço de frete está rodando na porta 3333.");
+});
