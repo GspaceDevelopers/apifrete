358b735291534d0473a34430cb8c209459fd8f01
Gspace Developers <115160413+GspaceDevelopers@users.noreply.github.com>
2023-02-09 13:21:55 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import cors from "cors";
 export default (app) => {
   app.use("/cep", require("./routes/cep"));
   app.use("/frete", require("./routes/frete"));
-  app.use(cors("Access-Control-Allow-Origin"));
+  app.use(cors());
 };
